6db2f27d9fc4294efd09c7807d940511b0b08c56
Gajendra Raj Sharma <47485482+Gajju45@users.noreply.github.com>
2020-07-03 15:28:05 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Arithmetic{
+public static void main(String args[]){
+
+//initialize
+int add,sub,mul;
+float div;
+int num1=15,num2=7;
+add=num1+num2;
+sub=num1-num2;
+mul=num1*num2;
+div=num1/num2;
+//Print Values
+System.out.println("Addition is " +add);
+System.out.println(" Subtraction is" + sub);
+System.out.println(" Multiplication is" + mul);
+System.out.println(" Division  is" + div);
+}}
