30a548a1a02f36e80eb2c103899c044f90ed4406
Gajendra Raj Sharma <47485482+Gajju45@users.noreply.github.com>
2020-07-04 15:22:57 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+class Swap{
+
+public static void main(String arg[])
+{
+int a=20,b=10;
+System.out.print("Before Swap\n");
+System.out.print("a="+a +"\n"+ "b="+b);
+//without Third Variable
+a=a+b;//a=30
+b=a-b;//b=30-10=20
+a=a-b;//30-20=10
+
+
+System.out.print("\nAfter Swap\n");
+System.out.print("a="+a +"\n"+ "b="+b);
+}}
+
+//with Third Variable
+//c=a;
+//a=b;
+//b=c;
